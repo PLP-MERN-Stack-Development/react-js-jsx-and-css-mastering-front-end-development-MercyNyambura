bad34d4066f71f3ab7124e6e3de0911be8618866
MercyNyambura <mercynyambura274@gmail.com>
2025-11-19 14:35:07 +0300

    changed several files

--- src/pages/utils/api.js
+++
@@ -1,9 +0,0 @@
-// utils/api.js
-
-export const API_URL = "https://jsonplaceholder.typicode.com/posts";
-
-export const fetchPosts = async (page = 1, limit = 9) => {
-  const res = await fetch(`${API_URL}?_page=${page}&_limit=${limit}`);
-  if (!res.ok) throw new Error("Failed to fetch posts");
-  return await res.json();
-};
